eb3a6d035bc639658f9d6ece2982624d3c3ab32a
Ruben Ramirez <rj@allvalleyservicesca.com>
2024-07-29 11:48:00 -0500

    ssl comment out

--- database.py
+++ database.py
@@ -11,8 +11,8 @@ dbPass = os.getenv("DB_PASS")
 dbHost = "dbportfolio.cvoieyqw6d46.us-east-2.rds.amazonaws.com"
 dbPort = "3306"
 dbName = "db_portfolio"
-ssl_conn ="&ssl_ca=/home/gord/client-ssl/ca.pem&ssl_cert=/home/gord/client-ssl/client-cert.pem&ssl_key=/home/gord/client-ssl/client-key.pem&ssl_check_hostname=false"
-db_connection = f"mysql+pymysql://{dbUser}:{dbPass}@{dbHost}:{dbPort}/{dbName}?charset=utf8mb4{ssl_conn}"
+#ssl_conn ="&ssl_ca=/home/gord/client-ssl/ca.pem&ssl_cert=/home/gord/client-ssl/client-cert.pem&ssl_key=/home/gord/client-ssl/client-key.pem&ssl_check_hostname=false"
+db_connection = f"mysql+pymysql://{dbUser}:{dbPass}@{dbHost}:{dbPort}/{dbName}?charset=utf8mb4"
 
 engine = create_engine(db_connection,connect_args={'connect_timeout':10})
 Session = sessionmaker(bind=engine)
